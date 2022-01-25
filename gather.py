490cf717507cefe24f0e32247516f50886e9fbbd
Nil Pujol <nilpujolporta@gmail.com>
2022-01-25 10:23:46 +0100

    començ de substitucio de .bats per .py

---
+++ gather.py
@@ -0,0 +1,8 @@
+from SynologyAPI import synology_API
+from catbackupAPI import CatbackupAPI_NPP
+from PandoraAPI import PandoraFMS_API
+
+PandoraFMS_API.main()
+CatbackupAPI_NPP.main()
+synology_API.main()
+
