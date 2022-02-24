3799f7922d5c4800fb1f5e3ee93c004868794bd0
Nil Pujol <nilpujolporta@gmail.com>
2022-02-24 09:59:19 +0100

    v0.2.4 | Added support for portable chrome

--- gather/gather.py
+++ gather/gather.py
@@ -1,3 +1,4 @@
+from pickle import NONE
 from SynologyAPI import synology_API
 from catbackupAPI import CatbackupAPI_NPP
 from PandoraAPI import PandoraFMS_API
@@ -6,10 +7,11 @@ from refresher import refresher
 import argparse
 import wget
 
-__version__= "0.2.3"
+__version__= "0.2.4"
 def main(args=None):
     
     parser = argparse.ArgumentParser(description='Serveix per actualitzar dashboard de PowerBI desktop localment.')
+    parser.add_argument('--portable-chrome-path', help="La ruta del executable de chrome", default=NONE, metavar="RUTA")
     parser.add_argument('-q', '--quiet', help='Nomes mostra els errors i el missatge de acabada per pantalla.', action="store_false")
     parser.add_argument('-v', '--versio', help='Mostra la versio', action='version', version='refresh-PowerBI v'+__version__)
     args = parser.parse_args(args)
@@ -27,14 +29,14 @@ def main(args=None):
     rutaPBIX="C:\\ns/apis.pbix"
     if args.quiet:
         PandoraFMS_API.main(['-q','-e','-f',rutaExcelPandora,'--json-file',rutaJSONPandora])
-        CatbackupAPI_NPP.main(['-q','--json-file',rutaJSONCatbackup])#'--portable-chrome-path', rutaChromePortable
+        CatbackupAPI_NPP.main(['-q','--json-file',rutaJSONCatbackup,'--portable-chrome-path', args.portable_chrome_path])
         synology_API.main(['-q','-e','--json-file',rutaJSONSynology,'-f',rutaExcelSynology])
-        HyperbackupAPI2_NPP.main(['-q','--json-file',rutaJSONHyperbackup2])#'--portable-chrome-path', rutaChromePortable
+        HyperbackupAPI2_NPP.main(['-q','--json-file',rutaJSONHyperbackup2,'--portable-chrome-path', args.portable_chrome_path])
     else:
         PandoraFMS_API.main(['-e','-f',rutaExcelPandora,'--json-file',rutaJSONPandora])
-        CatbackupAPI_NPP.main(['--json-file',rutaJSONCatbackup])
+        CatbackupAPI_NPP.main(['--json-file',rutaJSONCatbackup,'--portable-chrome-path', args.portable_chrome_path])
         synology_API.main(['-e','--json-file',rutaJSONSynology,'-f',rutaExcelSynology])
-        HyperbackupAPI2_NPP.main(['--json-file',rutaJSONHyperbackup2])#'--portable-chrome-path', rutaChromePortable
+        HyperbackupAPI2_NPP.main(['--json-file',rutaJSONHyperbackup2,'--portable-chrome-path', args.portable_chrome_path])
     try:
         refresher.main(['-f',rutaPBIX])
     except:
